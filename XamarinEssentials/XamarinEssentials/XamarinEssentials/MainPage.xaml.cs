d028ee6bf08bb885c01f1829343251091e2dec58
Tsurcan Alexandr <Brain.Cawa@gmail.com>
2023-05-18 10:41:56 +0300

    init

---
+++ XamarinEssentials/XamarinEssentials/XamarinEssentials/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinEssentials;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+
+        slider.Value = 0.5;
+
+        switch (Device.RuntimePlatform)
+        {
+            case Device.iOS:
+                new Thickness(0, 20, 0, 0);
+                break;
+            default:
+                new Thickness(0, 0, 0, 0);
+                break;
+        }
+    }
+}
---
+++ XamarinEssentials/XamarinEssentials/XamarinEssentials/QuotesPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinEssentials
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class QuotesPage : ContentPage
+    {
+        private int _index = 0;
+        private readonly string[] _qoutes =
+        {
+            "If you can't explain it simply, you don't understand it well enough.",
+            "We cannot solve our problems with the same thinking we used when we created them.",
+            "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."
+        };
+
+        public QuotesPage()
+        {
+            InitializeComponent();
+            currentQuote.Text = _qoutes[_index];
+        }
+
+        private void NextQuote(object sender, System.EventArgs e)
+        {
+            _index++;
+
+            if (_index >= _qoutes.Length) _index = 0;
+
+            currentQuote.Text = _qoutes[_index];
+        }
+    }
+}
