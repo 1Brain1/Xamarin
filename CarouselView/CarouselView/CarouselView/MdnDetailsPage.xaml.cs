69903236ccfccd0a0449cd31d560db6a04be32a1
Tsurcan Alexandr <Brain.Cawa@gmail.com>
2023-06-26 21:51:35 +0300

    to previous

--- CarouselView/CarouselView/CarouselView/MdnDetailsPage.xaml.cs
+++ CarouselView/CarouselView/CarouselView/MdnDetailsPage.xaml.cs
@@ -14,7 +14,7 @@ public partial class MdnDetailsPage : ContentPage
     {
         InitializeComponent();
 
-        carouselView.ItemsSource = GetMdns();
+        carouselView.ItemsSource = GetPurchases();
     }
 
     public async void Test(object sender, EventArgs e)
@@ -22,40 +22,40 @@ public partial class MdnDetailsPage : ContentPage
         await DisplayAlert("Attention", "Wrong Email or Password", "Ok");
     }
 
-    private ObservableCollection<MdnDto> GetMdns()
+    private ObservableCollection<Purchases> GetPurchases()
     {
-        return new ObservableCollection<MdnDto>
+        return new ObservableCollection<Purchases>
         {
-            new MdnDto
+            new Purchases
             {
-                MndNumber = "3304324630",
+                Mdn = "3304324630",
                 Sim = "89148000005758879720",
                 Imei = "1535500225374",
                 Provider = "Red",
                 IsActive = true
             },
 
-            new MdnDto
+            new Purchases
             {
-                MndNumber = "7172501308",
+                Mdn = "7172501308",
                 Sim = "89148000005758880256",
                 Imei = "990007043121315",
                 Provider = "Purple",
                 IsActive = false
             },
 
-            new MdnDto
+            new Purchases
             {
-                MndNumber = "4846609115",
+                Mdn = "4846609115",
                 Sim = "8901260853187564503",
                 Imei = "352051374249487",
                 Provider = "Purple",
                 IsActive = true
             },
 
-            new MdnDto
+            new Purchases
             {
-                MndNumber = "4847725148",
+                Mdn = "4847725148",
                 Sim = "",
                 Imei = "352051374614417",
                 Provider = "Red",
