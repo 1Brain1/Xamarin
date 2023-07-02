af7980235596570568d680c4846dd8d581818a72
Tsurcan Alexandr <Brain.Cawa@gmail.com>
2023-07-03 01:09:43 +0300

    Profile page fixed

---
+++ CarouselView/CarouselView/CarouselView/Converters/Base64ToImageConverter.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using System.IO;
+using System;
+using Xamarin.Forms;
+
+namespace CarouselView.Converters;
+
+class Base64ToImageConverter : IValueConverter
+{
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        try
+        {
+            var byteArray = System.Convert.FromBase64String((string)value);
+            Stream stream = new MemoryStream(byteArray);
+            var imageSource = ImageSource.FromStream(() => stream);
+            return imageSource;
+        }
+        catch
+        {
+            return value;
+        }
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
