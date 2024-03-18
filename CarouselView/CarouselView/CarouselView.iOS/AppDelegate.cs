fdfa72fbae22991ad9c710ac8f55025aa1b6d288
Tsurcan Alexandr <Brain.Cawa@gmail.com>
2024-03-18 17:10:31 +0200

    iOS project added

---
+++ CarouselView/CarouselView/CarouselView.iOS/AppDelegate.cs
@@ -0,0 +1,23 @@
+﻿using Foundation;
+using UIKit;
+
+namespace CarouselView.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        // class-level declarations
+
+        public override UIWindow Window { get; set; }
+
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
--- CarouselView/CarouselView/CarouselView.iOS/Properties/AssemblyInfo.cs
+++ CarouselView/CarouselView/CarouselView.iOS/Properties/AssemblyInfo.cs
@@ -5,15 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CarouselView1.iOS")]
+[assembly: AssemblyTitle("CarouselView.iOS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("CompanyName")]
-[assembly: AssemblyProduct("CarouselView1.iOS")]
-[assembly: AssemblyCopyright("Copyright © CompanyName Year")]
-[assembly: AssemblyTrademark("CompanyTrademark")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CarouselView.iOS")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("496C7D8E-9BDC-447A-94CC-88159BA75D25")]
 
 // Version information for an assembly consists of the following four values:
 //
