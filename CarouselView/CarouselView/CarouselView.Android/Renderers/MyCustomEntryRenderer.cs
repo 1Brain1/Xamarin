ba5f22c5efde93e9d8a552f6ba42ef1a11d8fb76
Tsurcan Alexandr <Brain.Cawa@gmail.com>
2024-03-21 20:48:49 +0200

    Android: renderers added

---
+++ CarouselView/CarouselView/CarouselView.Android/Renderers/MyCustomEntryRenderer.cs
@@ -0,0 +1,45 @@
+﻿using Android.Content;
+using Android.Graphics.Drawables;
+using CarouselView.Controls;
+using CarouselView.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Color = Android.Graphics.Color;
+using TextAlignment = Android.Views.TextAlignment;
+
+[assembly: ExportRenderer(typeof(MyEntry), typeof(MyCustomEntryRenderer))]
+
+namespace CarouselView.Droid.Renderers
+{
+    public class MyCustomEntryRenderer : EntryRenderer
+    {
+        public MyCustomEntryRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null) return;
+            Control.SetBackgroundResource(Color.Transparent);
+            Control.SetBackgroundColor(Color.Transparent);
+            Control.SetPadding(0, 0, 0, 0);
+            Control.TextAlignment = TextAlignment.ViewStart;
+            Control.SetTextColor(Color.Black);
+            Control.SetHighlightColor(Color.Gray);
+
+
+            var gd = new GradientDrawable();
+            gd.SetColor(Color.White);
+            gd.SetCornerRadius(10);
+            gd.SetStroke(2, Color.LightGray);
+
+
+#pragma warning disable 618
+            Control.SetBackgroundDrawable(gd);
+#pragma warning restore 618
+            Control.SetHintTextColor(Color.Gray);
+        }
+    }
+}
---
+++ CarouselView/CarouselView/CarouselView.Android/Renderers/MyEntryRenderer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CarouselView.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Color = Android.Graphics.Color;
+using TextAlignment = Android.Views.TextAlignment;
+
+#pragma warning disable CS0612 // Type or member is obsolete
+[assembly: ExportRenderer(typeof(Entry), typeof(MyEntryRenderer))]
+#pragma warning restore CS0612 // Type or member is obsolete
+namespace CarouselView.Droid.Renderers
+{
+    [Obsolete]
+    public class MyEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null) return;
+            Control.SetBackgroundResource(Color.Transparent);
+            Control.SetBackgroundColor(Color.Transparent);
+            Control.SetPadding(0, 0, 0, 0);
+            Control.TextAlignment = TextAlignment.TextStart;
+            Control.SetTextColor(Color.Black);
+        }
+    }
+}
